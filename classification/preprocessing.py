f8ba8f1e83bbd35a59e922166d7f1cd712a46ac5
Than Van Con <mtvc95@gmail.com>
2018-10-18 16:59:23 +0200

    Adding a saving function to preprosssing script

--- classification/preprocessing.py
+++ classification/preprocessing.py
@@ -1,44 +1,3 @@
-# # preprocessing.py by Mosquito boys project
-#
-# from google.cloud import vision
-# import os
-# from pprint import pprint
-#
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./google-cloud-credentials.json"
-#
-#
-# def localize_objects(image_path):
-#     """Localize objects in the local image.
-#
-#     Args:
-#     path: The path to the local file.
-#     """
-#     client = vision.ImageAnnotatorClient()
-#
-#
-#     with open(image_path, 'rb') as image_file:
-#         content = image_file.read()
-#     image = vision.types.Image(content=content)
-#
-#     objects = client.object_localization(
-#         image=image).localized_object_annotations
-#
-#     pprint(objects)
-#     return objects
-#
-#
-# def localize_mosquitoes(image_path):
-#     objects = localize_objects(image_path)
-#     print('Number of objects found: {}'.format(len(objects)))
-#     for object_ in objects:
-#         print('\n{} (confidence: {})'.format(object_.name, object_.score))
-#         print('Normalized bounding polygon vertices: ')
-#         for vertex in object_.bounding_poly.normalized_vertices:
-#             print(' - ({}, {})'.format(vertex.x, vertex.y))
-#
-#
-# localize_mosquitoes('dataset/aedes/pic_001.jpg')
-
 # Libraries and environment setting
 import requests
 import base64
@@ -52,12 +11,17 @@ env = Env()
 env.read_env()  # read .env file, if it exists
 
 
-class Preprocessing():
+
+class Preprocessing:
+    """
+    """
 
     @staticmethod
     def mosquito_position(image_path):
-        # retrieves coordinates of 4 points in the image framing the mosquito
-        # (image width pct for x,  image length pct for y)
+        """
+        retrieves coordinates of 4 points in the image framing the mosquito
+        (image width pct for x,  image length pct for y)
+        """
         with open(image_path, 'rb') as image_file:
             content = base64.encodebytes(image_file.read())
 
@@ -102,7 +66,9 @@ class Preprocessing():
 
     @staticmethod
     def mosquito_croping(coords, image_path):
-        # crops the image around the mosquito and resizes the image into a square
+        """
+        crops the image around the mosquito and resizes the image into a square
+        """
         img = cv2.imread(image_path)
 
         pt1 = (int(coords[0]["x"] * len(img[0])), int(coords[0]["y"] * len(img)))
@@ -116,6 +82,9 @@ class Preprocessing():
 
     @staticmethod
     def mosquito_framing(coords, image_path):
+        """
+
+        """
         # appends a black rectangle around the mosquito
         img = cv2.imread(image_path)
 
@@ -125,6 +94,10 @@ class Preprocessing():
         cv2.rectangle(img, pt1, pt2, (0, 0, 0), thickness=1, lineType=8, shift=0)
 
         return img
+
+    @staticmethod
+    def save_img(img, path):
+        cv2.imwrite(path, img)
 
 ##Test
 

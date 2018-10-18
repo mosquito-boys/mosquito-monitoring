37875ffe345809c73df02e62ab65b5322b4911b0
Paul <paul.asquin@gmail.com>
2018-10-18 18:01:23 +0200

    add test of the command_retrain script

---
+++ test/test_command_retrain.py
@@ -0,0 +1,10 @@
+# testing command_retrain.py
+import classification.command_retrain as command_retrain
+
+
+def test_command_retrain():
+    command_retrain.main()
+
+
+if __name__ == "__main__":
+    test_command_retrain()

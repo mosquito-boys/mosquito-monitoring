17b59c7e6fb35468828bee5450edc4b01fa82677
Than Van Con <mtvc95@gmail.com>
2018-10-18 17:35:49 +0200

    Adding a test dir with test of preprocessing.py and update of gitignore

---
+++ test/test_preprocessing.py
@@ -0,0 +1,16 @@
+import os
+import classification.preprocessing as pre
+import cv2
+
+image_path = 'dataset/aedes/pic_009.jpg'
+
+coords = pre.Preprocessing.mosquito_position(image_path)
+
+framed = pre.Preprocessing.mosquito_framing(coords, image_path)
+crop_resized = pre.Preprocessing.mosquito_croping(coords, image_path)
+
+cv2.imshow('framed', framed)
+cv2.waitKey(0)
+
+cv2.imshow('crop_resized', crop_resized)
+cv2.waitKey(0)

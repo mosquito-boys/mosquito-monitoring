b9370e976951b95862f2d2ad9da889142521b0e3
Victor Aubin <aubin.victor@gmail.com>
2018-10-26 15:35:28 +0200

    fix 4 heroku

--- utilities/EnvReader.py
+++ utilities/EnvReader.py
@@ -1,15 +1,16 @@
 from utilities.Errors import EnvError
 import os
 
-try:
-    with open('.env') as f:
-        env_arr = f.readlines()
+if os.exists(".env"):
+    try:
+        with open('.env') as f:
+            env_arr = f.readlines()
 
-    for couple in env_arr:
-        os.environ[couple.split("=")[0].strip()] = couple.split("=")[1].strip()
+        for couple in env_arr:
+            os.environ[couple.split("=")[0].strip()] = couple.split("=")[1].strip()
 
-except Exception:
-    raise EnvError()
+    except Exception:
+        raise EnvError()
 
 
 def get_api_key():

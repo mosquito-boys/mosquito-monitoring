3446b7b2bcc41df4abfdfbade450a23f2e6d03eb
Victor Aubin <aubin.victor@gmail.com>
2018-11-11 14:53:51 +0100

    typo fix

--- db_model/DBEngine.py
+++ db_model/DBEngine.py
@@ -7,7 +7,7 @@ class DBEngine(ABC):
     This class has only static methods to be called freely from anywhere in the project without needing to create objects.
 
     (public static) create_database: create the database if it don't exist.
-    (public static) drop_datase: delete the database
+    (public static) drop_database: delete the database
     (public static) get_all_mosquitos: perform the adequate query to the DB to get all mosquitos sorted by species
     (public static) store_mosquito: insert the mosquito instance to the DB
     """
